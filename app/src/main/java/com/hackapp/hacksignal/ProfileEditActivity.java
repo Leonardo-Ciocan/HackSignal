92b70eec800bf31d1fa75bb31d5613dbdd4840a0
luiz <lzcoccante@gmail.com>
2015-10-10 14:09:44 +0100

    Profile Edit Working

--- app/src/main/java/com/hackapp/hacksignal/ProfileEditActivity.java
+++ app/src/main/java/com/hackapp/hacksignal/ProfileEditActivity.java
@@ -1,17 +1,40 @@
 package com.hackapp.hacksignal;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ProfileEditActivity extends ActionBarActivity {
+
+    public EditText languageEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit);
+
+        languageEdit = (EditText) findViewById(R.id.editTextLanguage);
+
+
+        String[] languages = {"Java","C#","C++","C","Ruby","Phyton"};
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.item,languages);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ItemList());
+
     }
 
     @Override
@@ -34,5 +57,41 @@ public class ProfileEditActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public Boolean checkLanguage(String languageName){
+
+        return languageEdit.getText().toString().contains(languageName);
+
+    }
+
+    public void showDialog(String msg){
+        AlertDialog alertDialog = new AlertDialog.Builder(ProfileEditActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+
+    class ItemList implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            ViewGroup vg = (ViewGroup) view;
+            TextView tv = (TextView) vg.findViewById(R.id.item);
+            Toast.makeText(ProfileEditActivity.this,tv.getText().toString(),Toast.LENGTH_SHORT).show();
+            if(!checkLanguage(tv.getText().toString())){
+                languageEdit.setText(languageEdit.getText() + tv.getText().toString()+ ";");
+            }else{
+                showDialog("This Language is Already on the List");
+            }
+
+        }
     }
 }
